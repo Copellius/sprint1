42c7c64009d19b7b6535f8e299874ae3f1bf518a
MarceloDono <marcelo@MacBook-Air-de-Marcelo.local>
2025-12-15 13:47:58 +0100

    comienzo, ya todo enlazado, mixin de botones

---
+++ iteracion-2/main.js
@@ -0,0 +1,25 @@
+"use strict"
+
+function showSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
+
+function showFeature(which) {
+  const one = document.querySelector(".feature-one");
+  const two = document.querySelector(".feature-two");
+  const three = document.querySelector(".feature-three");
+
+  one.classList.remove("active");
+  two.classList.remove("active");
+  three.classList.remove("active");
+
+  if (which === 1) one.classList.add("active");
+  if (which === 2) two.classList.add("active");
+  if (which === 3) three.classList.add("active");
+}
