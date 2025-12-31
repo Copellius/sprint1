9fab4df6cc9c9e2fb66768c23ccba07237874829
MarceloDono <marcelo@MacBook-Air-de-Marcelo.local>
2025-12-31 11:23:43 +0100

    features js functionality fixed, anchors to do next

--- main.js
+++ main.js
@@ -11,15 +11,15 @@ function hideSidebar(){
 }
 
 function showFeature(which) {
-  const one = document.querySelector(".feature-one");
-  const two = document.querySelector(".feature-two");
-  const three = document.querySelector(".feature-three");
+  const features = document.querySelectorAll(
+    ".feature-one, .feature-two, .feature-three"
+  );
 
-  one.classList.remove("active");
-  two.classList.remove("active");
-  three.classList.remove("active");
+  features.forEach(feature => {
+    feature.classList.add("hidden");
+  });
 
-  if (which === 1) one.classList.add("active");
-  if (which === 2) two.classList.add("active");
-  if (which === 3) three.classList.add("active");
+  if (which === 1) document.querySelector(".feature-one").classList.remove("hidden");
+  if (which === 2) document.querySelector(".feature-two").classList.remove("hidden");
+  if (which === 3) document.querySelector(".feature-three").classList.remove("hidden");
 }
