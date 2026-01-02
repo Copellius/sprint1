c83c01fffd51359368329ba4c1c6679960b1fdf3
MarceloDono <marcelo@MacBook-Air-de-Marcelo.local>
2026-01-02 19:06:29 +0100

    inicio de version desktop, trabajando en los features

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 "use strict"
 
-function showSidebar(){
-    const sidebar = document.querySelector(".sidebar")
-    sidebar.style.display = "flex"
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.remove("hidden");
+  sidebar.classList.add("flex");
 }
 
-function hideSidebar(){
-    const sidebar = document.querySelector(".sidebar")
-    sidebar.style.display = "hidden"
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.add("hidden");
+  sidebar.classList.remove("flex");
 }
+
 
 function showFeature(which) {
   const features = document.querySelectorAll(
@@ -22,4 +25,13 @@ function showFeature(which) {
   if (which === 1) document.querySelector(".feature-one").classList.remove("hidden");
   if (which === 2) document.querySelector(".feature-two").classList.remove("hidden");
   if (which === 3) document.querySelector(".feature-three").classList.remove("hidden");
+}
+
+function setActive(tab) {
+
+  document.querySelectorAll("h3 span.bg-red-500").forEach(line => {
+    line.classList.add("hidden");
+  });
+
+  tab.querySelector("span.bg-red-500").classList.remove("hidden");
 }
